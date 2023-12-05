d3a763140ff8abf4a1e162ae4d406017d8fb4416
Raju George <test@example.com>
2023-12-05 12:17:44 +0530

    candidate loading and empty cities fixed

--- app/(admin)/admin/(main)/reports/components/candidates.tsx
+++ app/(admin)/admin/(main)/reports/components/candidates.tsx
@@ -1,19 +1,24 @@
 "use client";
 import { getAllCandidatesInfo } from "@/app/data/reports/candidate";
 import { ArrowDownOnSquareIcon } from "@heroicons/react/24/outline";
-import React from "react";
+import React, { useState } from "react";
 import FileDownload from "js-file-download";
+import DataLoader from "@/app/components/DataLoader";
 
 const Candidates = () => {
+  const [isFetching, setIsFetching] = useState(false);
+
   async function handleExcelDownload() {
+    setIsFetching(true);
     try {
       const data = await getAllCandidatesInfo();
-      console.log(data);
 
       const filename = `All-Candidates-${Date.now()}.xlsx`;
 
       FileDownload(data, filename);
+      setIsFetching(false);
     } catch (error) {
+      setIsFetching(false);
       console.error("Error downloading Excel file:", error);
     }
   }
@@ -29,11 +34,19 @@ const Candidates = () => {
         <div>
           <button
             type="button"
+            disabled={isFetching}
             className="flex gap-1 items-center rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
             onClick={handleExcelDownload}
           >
-            <ArrowDownOnSquareIcon className="h-6 w-6" />
-            Download Report
+            {isFetching ? (
+              <DataLoader />
+            ) : (
+              <>
+                {" "}
+                <ArrowDownOnSquareIcon className="h-6 w-6" />
+                Download Report
+              </>
+            )}
           </button>
         </div>
       </div>
